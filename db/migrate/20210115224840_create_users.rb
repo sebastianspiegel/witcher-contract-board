dd57ff0544c51121f6b8540801cf95c31f59152a
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-16 11:00:22 -0500

    updating relationship and model for witcherscontracts

--- db/migrate/20210115224840_create_users.rb
+++ db/migrate/20210115224840_create_users.rb
@@ -3,8 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-      t.string :school
-      t.boolean :witcher, default: 0 
+      t.string :school, default: nil 
 
       t.timestamps
     end
