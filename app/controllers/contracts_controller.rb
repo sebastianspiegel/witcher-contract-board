d8e10fa0a5fec8d9f8416e07d0f347c48b0c5bce
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-18 15:08:13 -0500

    creating a new contract with new monster and location working

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -5,24 +5,27 @@ class ContractsController < ApplicationController
     end
 
     def create
-        byebug
-        contract = Contract.new(details: params[:contract][:details], reward: params[:contract][:reward])
-        if params[:contract][:monster_id] != 1
-            contract.monster_id = params[:contract][:monster_id]
+        @contract = Contract.new(details: params[:contract][:details], reward: params[:contract][:reward], user_id: session[:user_id])
+        if params[:contract][:monster_id] != "1"
+            @contract.monster_id = params[:contract][:monster_id]
         else
-            contract.monster = Monster.create(name: params[:contract][:monster][:name], type_id: params[:contract][:mosnter][:type_id])
+            @contract.monster = Monster.create(name: params[:contract][:monster][:name], type_id: params[:contract][:monster][:type_id])
         end
-        if params[:contract][:location_id] != 1
-            contract.location_id = params[:contract][:location_id]
+        if params[:contract][:location_id] != "1"
+            @contract.location_id = params[:contract][:location_id]
         else
-            contract.location = Location.create(name: params[:contract][:location][:name])
+            @contract.location = Location.create(name: params[:contract][:location][:name])
         end
-        contract.save 
-        redirect_to contract_path(contract.id)
+        if @contract.save 
+            redirect_to contract_path(@contract.id)
+        else 
+            render :new
+        end
     end
 
     def show
         @contract = Contract.find(params[:id])
+        @user = User.find(session[:user_id])
     end
 
     def index 
