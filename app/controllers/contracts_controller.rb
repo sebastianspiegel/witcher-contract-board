4fab5bad2f5e36dca3a5bb7017325a75c5893dbc
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-18 10:30:21 -0500

    controller and views for contracts index

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -7,9 +7,17 @@ class ContractsController < ApplicationController
     end
 
     def show
+        @contract = Contract.find(params[:id])
     end
 
     def index 
+        @contracts = Contract.all 
+    end
+
+    def edit
+    end
+
+    def update
     end
 
 end
