66fb9516330b9f850ba151103d78b31e4dd98330
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-19 17:17:12 -0500

    new scope for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,5 @@
 class LocationsController < ApplicationController
     def index
-        @locations = Location.all 
+        @locations = Location.all[1..-1]
     end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
 class Location < ApplicationRecord
     has_many :contracts 
     has_many :monsters, through: :contracts 
+
+    def most_prevelent_type
+        #most prevelent type of monster in location
+        # Location.monsters / each monsters type_id / count each type_id / order by ASC LIMIT 1
+    end
 end
