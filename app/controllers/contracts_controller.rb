52bb79a2a5169161737d0728da158fcc21c4fc36
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-19 09:32:55 -0500

    adding links in header and show pages

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -24,26 +24,6 @@ class ContractsController < ApplicationController
         end
     end
 
-    # def create
-    #     byebug
-    #     @contract = Contract.new(details: params[:contract][:details], reward: params[:contract][:reward], user_id: session[:user_id])
-    #     if params[:contract][:monster_id] != "1"
-    #         @contract.monster_id = params[:contract][:monster_id]
-    #     else
-    #         @contract.monster = Monster.create(name: params[:contract][:monster][:name], type_id: params[:contract][:monster][:type_id])
-    #     end
-    #     if params[:contract][:location_id] != "1"
-    #         @contract.location_id = params[:contract][:location_id]
-    #     else
-    #         @contract.location = Location.create(name: params[:contract][:location][:name])
-    #     end
-    #     if @contract.save 
-    #         redirect_to contract_path(@contract.id)
-    #     else 
-    #         render :new
-    #     end
-    # end
-
     def show
         @contract = Contract.find(params[:id])
         @user = User.find(session[:user_id])
@@ -60,12 +40,12 @@ class ContractsController < ApplicationController
     def update
         @contract = Contract.find(params[:id])
         @contract.update(contract_params)
+        redirect_to contract_path(@contract.id)
     end
 
     private
 
     def contract_params
-        # params.permit(:contract).require(:details, :reward, :monster_id, :monster, :location_id, :location)
         params.require(:contract).permit(:details, :reward, monster_attributes: [:name, :type_id], location_attributes: [:name])
     end
 
