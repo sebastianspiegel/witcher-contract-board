312320ef25db1319bc8878440a3a5cf05bcd9f5c
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-19 09:11:03 -0500

    new contract nested attributes working for creating new monster/location

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,27 +2,47 @@ class ContractsController < ApplicationController
 
     def new 
         @contract = Contract.new 
+        @contract.build_monster
+        @contract.build_location 
     end
 
     def create
-        byebug
-        @contract = Contract.new(details: params[:contract][:details], reward: params[:contract][:reward], user_id: session[:user_id])
-        if params[:contract][:monster_id] != "1"
-            @contract.monster_id = params[:contract][:monster_id]
-        else
-            @contract.monster = Monster.create(name: params[:contract][:monster][:name], type_id: params[:contract][:monster][:type_id])
+        @contract = Contract.new(contract_params)
+        @contract.user = current_user
+        if params[:monster_id]
+            @monster = Monster.find(params[:monster_id])
         end
-        if params[:contract][:location_id] != "1"
-            @contract.location_id = params[:contract][:location_id]
-        else
-            @contract.location = Location.create(name: params[:contract][:location][:name])
+
+        if params[:location_id]
+            @location = Location.find(params[:location_id])
         end
-        if @contract.save 
+
+        if @contract.save
             redirect_to contract_path(@contract.id)
-        else 
+        else
             render :new
         end
     end
+
+    # def create
+    #     byebug
+    #     @contract = Contract.new(details: params[:contract][:details], reward: params[:contract][:reward], user_id: session[:user_id])
+    #     if params[:contract][:monster_id] != "1"
+    #         @contract.monster_id = params[:contract][:monster_id]
+    #     else
+    #         @contract.monster = Monster.create(name: params[:contract][:monster][:name], type_id: params[:contract][:monster][:type_id])
+    #     end
+    #     if params[:contract][:location_id] != "1"
+    #         @contract.location_id = params[:contract][:location_id]
+    #     else
+    #         @contract.location = Location.create(name: params[:contract][:location][:name])
+    #     end
+    #     if @contract.save 
+    #         redirect_to contract_path(@contract.id)
+    #     else 
+    #         render :new
+    #     end
+    # end
 
     def show
         @contract = Contract.find(params[:id])
@@ -46,7 +66,11 @@ class ContractsController < ApplicationController
 
     def contract_params
         # params.permit(:contract).require(:details, :reward, :monster_id, :monster, :location_id, :location)
-        params.permit(:contract).require(:details, :reward, monster_attributes: [:monster], location_attributes: [:location])
+        params.require(:contract).permit(:details, :reward, monster_attributes: [:name, :type_id], location_attributes: [:name])
+    end
+
+    def current_user
+        User.find(session[:user_id])
     end
 
 end
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,6 +2,8 @@ class Contract < ActiveRecord::Base
     belongs_to :user
     belongs_to :monster
     belongs_to :location 
+    accepts_nested_attributes_for :monster
+    accepts_nested_attributes_for :location
 
     def claim_contract(user)
         self.claimed_id = user.id
