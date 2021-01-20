1a38213a79340ccb03a72da5e72a41a4868d89bd
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-20 15:25:55 -0500

    refactoring user views to remove logic to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,27 @@
 module UsersHelper
+
+    def claimed_or_unclaimed(contract)
+        if !contract.contract_is_claimed?
+            "Unclaimed"
+        else
+            "Claimed by: #{link_to User.find(contract.claimed_by.id).name, user_path(contract.claimed_by.id)}"
+        end
+        
+    end
+
+    def show_school
+        if @user.witcher? 
+            content_tag(:h3, "#{@user.school.name}") 
+        end
+    end
+
+    def claimed_or_posted_by
+        if @user.witcher? && @user = current_user
+            content_tag(:h4, "Claimed contracts:")
+        elsif !@user.witcher?
+            content_tag(:h4, "Posted contracts:")
+        end
+    end
     
 end
  
