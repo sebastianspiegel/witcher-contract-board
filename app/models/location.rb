491b034a05b816261a8e686b92357398c12a8dd9
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-21 10:51:59 -0500

    working on scope methods

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,13 +4,23 @@ class Location < ApplicationRecord
     validates :name, uniqueness: true, presence: true
 
     def most_prevelent_type
-        #most prevelent type of monster in location
+        #in THIS LOCATION which TYPE of monster is seen the most?
+
         # Location.monsters / each monsters type_id / count each type_id / order by ASC LIMIT 1
+        # SELECT * FROM monsters INNER JOIN contracts ON contracts.monster_id = monsters.id ORDER BY monsters.type_id
+        
+    end
+
+    def location_with_most_monsters
+        #which location has the most mosnters? 
+        # SELECT * FROM contracts INNER JOIN locations WHERE contracts.location_id = locations.id ORDER BY location_id
     end
 
     def richest_location
         #location with highest total rewards
         #select reward from contract where location == self 
+
+        Contract.where(:location_id => self.id).sum(:reward)
     end
 
     def regional_types
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,14 @@
 class Type < ApplicationRecord
     has_many :monsters 
+
+
+    # def most_prevelent_type
+    #     # Contract.joins(monster)on(monster_id)where(locatoin_id = self.id).count
+
+    #     # Contract.joins("INNER JOIN monsters ON monsters.")
+    #     # Monster.joins("INNER JOIN contracts ON contracts.monster_id = monsters.id WHERE contracts.location_id = ? AND monsters.type_id = ?")
+
+    #     # SELECT * FROM monsters INNER JOIN contracts ON contracts.monster_id = monsters.id ORDER BY monsters.type_id
+    # end
+
 end
