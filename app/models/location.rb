4ab3f28fab2be4637cb37178f5beb3ee86004ce6
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-22 10:16:37 -0500

    adding methods for finding and displaying types of monsters seen at a location

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,23 +6,24 @@ class Location < ApplicationRecord
 
     scope :richest_location, -> { find(Contract.richest_location.ids.first) }
 
+    scope :with_most_monsters, -> { find(Contract.location_with_most_monsters.first) }
+
     def most_prevelent_type
         #in THIS LOCATION which TYPE of monster is seen the most?
-
         # Location.monsters / each monsters type_id / count each type_id / order by ASC LIMIT 1
         # SELECT * FROM monsters INNER JOIN contracts ON contracts.monster_id = monsters.id ORDER BY monsters.type_id
-        
-    end
-
-    def self.with_most_monsters
-        #which location has the most mosnters? 
-        #SELECT location_id, COUNT (*) FROM contracts JOIN locations WHERE contracts.location_id = locations.id GROUP BY location_id ORDER BY COUNT(*) DESC LIMIT 1
-        Location.find(Contract.location_with_most_monsters.first)
     end
 
     def regional_types
+        self.contracts.map{|c| c.monster}.map{|m| m.type}.map{|t| t.name}
+    end
 
-        self.monsters.where 
+    def display_regional_types
+        if self.regional_types.count == 0
+            "No monsters here!"
+        else
+            self.regional_types.uniq.join(", ").pluralize
+        end
     end
     
 end
