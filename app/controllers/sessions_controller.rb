9eac31d0a447eec5289ae0a81256b3bfc6222bc6
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-05-19 12:27:28 -0400

    fix redirect for bad login, remove oauth login option, and remove edit link on contract show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
             redirect_to signin_path
         else
             @user = User.find_by(username: params[:user][:username])
-            if !@user.authenticate(params[:user][:password])
+            if @user.nil? || !@user.authenticate(params[:user][:password])
                 redirect_to signin_path
             else
                 session[:user_id] = @user.id 
