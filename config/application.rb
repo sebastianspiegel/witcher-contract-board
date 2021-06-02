d2f353c5646e082ed3a8a667e2b9882f6a00b6aa
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-06-02 16:47:18 -0400

    remove bootswatch.rb and init precompile line

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
-config.assets.initialize_on_precompile = false
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,6 +12,8 @@ module WitcherContractBoard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    # config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
